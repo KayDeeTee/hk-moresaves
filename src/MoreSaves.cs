518feab2a2de4b890a2aa817ffbc8b3b0973fb99
KayDeeTee <kristian._thorpe@hotmail.co.uk>
2017-11-10 21:14:51 +0000

    Initial commit

---
+++ src/MoreSaves.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Modding;
+using GlobalEnums;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.UI;
+using System.Reflection;
+using UnityEngine.SceneManagement;
+
+namespace MoreSaves
+{
+    public class MoreSaves : Mod
+    {
+
+        private static string version = "0.2.0";
+
+        public override string GetVersion()
+        {
+            return version;
+        }
+
+        public static GameObject canvas;
+        public static Text pageLabel;
+        public static RectTransform rect;
+
+        public static Font trajanBold;
+        public static Font trajanNormal;
+
+        public static GameObject createTextPanel(GameObject parent, string defaultText, int x, int y, int w, int h, int s)
+        {
+            GameObject panel = new GameObject();
+            panel.transform.parent = parent.transform;
+            panel.AddComponent<CanvasRenderer>();
+            RectTransform rt = panel.AddComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(w, h);
+            rt.anchorMax = new Vector2(0, 0);
+            rt.anchorMin = new Vector2(0, 0);
+            rt.anchoredPosition = new Vector2(x, y);
+            UnityEngine.UI.Text text = panel.AddComponent<UnityEngine.UI.Text>();
+            text.font = trajanBold;
+            text.text = defaultText;
+            text.fontSize = s;
+            text.alignment = TextAnchor.MiddleCenter;
+            return panel;
+        }
+
+        public static GameObject createCanvas(int w, int h)
+        {
+            GameObject c = new GameObject();
+            c.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            CanvasScaler cs = c.AddComponent<CanvasScaler>();
+            cs.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            cs.referenceResolution = new Vector2(w, h);
+            c.AddComponent<GraphicRaycaster>();
+            return c;
+        }
+
+
+        public override void Initialize()
+        {
+            ModHooks.ModLog("Initializing MoreSaves");
+
+            UIManager.instance.gameObject.AddComponent<MoreSavesComponent>();
+
+            foreach (UnityEngine.Font f in UnityEngine.Resources.FindObjectsOfTypeAll<Font>())
+            {
+                if (f != null && f.name == "TrajanPro-Bold")
+                {
+                    trajanBold = f;
+                }
+
+                if (f != null && f.name == "TrajanPro-Regular")
+                {
+                    trajanNormal = f;
+                }
+            }
+
+            canvas = createCanvas(1920,1080);
+            pageLabel = createTextPanel(canvas, "Page 1/?", 1240, 870, 200, 200, 29).GetComponent<Text>();
+            pageLabel.enabled = false;
+
+            MonoBehaviour.DontDestroyOnLoad(canvas);
+
+            UnityEngine.SceneManagement.SceneManager.sceneLoaded += checkComponent;
+
+            ModHooks.ModLog("Initialized MoreSaves");
+        }
+
+        public void checkComponent(Scene scene, LoadSceneMode lsm){
+            if(UIManager.instance.gameObject.GetComponent<MoreSavesComponent>() == null)
+                UIManager.instance.gameObject.AddComponent<MoreSavesComponent>();
+        }
+
+        
+
+    }
+}
---
+++ src/MoreSavesComponent.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using Modding;
+using System.IO;
+using System;
+namespace MoreSaves
+{
+    class MoreSavesComponent : MonoBehaviour
+    {
+        private static GameManager gm;
+        private static UIManager uim;
+
+        private static int currentPage = 0;
+        private static int minPages = 2;
+        private static int maxPages;
+
+        private static float lastPageTransition;
+        private static float transistionTime = 0.5f;
+
+        private static bool setupHooks = false;
+
+        public void Start()
+        {
+            gm = GameManager.instance;
+            uim = UIManager.instance;
+
+            maxPages = PlayerPrefs.GetInt("MaxPages", 1);
+            if (maxPages < minPages)
+                maxPages = minPages;
+
+            MoreSaves.pageLabel.text = String.Format("Page {0}/{1}", currentPage+1, maxPages);
+
+            if (!setupHooks)
+            {
+                ModHooks.Instance.GetSaveFileNameHook += getFilename;
+                ModHooks.Instance.SavegameSaveHook += checkAddMaxPages;
+                ModHooks.Instance.SavegameClearHook += checkRemoveMaxPages;
+                setupHooks = true;
+            }
+
+        }
+
+        public void Update()
+        {
+            MoreSaves.pageLabel.enabled = uim.menuState == GlobalEnums.MainMenuState.SAVE_PROFILES;
+
+            if (uim.menuState == GlobalEnums.MainMenuState.SAVE_PROFILES && Time.time - lastPageTransition > transistionTime * 2)
+            {
+                bool updateSaves = false;
+                if (gm.inputHandler.inputActions.paneRight.WasPressed)
+                {
+                    currentPage++;
+                    updateSaves = true;
+                }
+                if (gm.inputHandler.inputActions.paneLeft.WasPressed)
+                {
+                    currentPage--;
+                    updateSaves = true;
+                }
+                currentPage = currentPage % maxPages;
+                if (currentPage < 0)
+                    currentPage = maxPages - 1;
+
+                if (updateSaves)
+                {
+                    MoreSaves.pageLabel.text = String.Format("Page {0}/{1}", currentPage + 1, maxPages);
+                    lastPageTransition = Time.time;
+                    //Instantly
+                    hideAllSaves();
+
+                    //After all faded
+                    Invoke("showAllSaves", transistionTime);
+                }
+            }
+        }
+
+        public void hideAllSaves()
+        {
+            uim.slotOne.HideSaveSlot();
+            uim.slotTwo.HideSaveSlot();
+            uim.slotThree.HideSaveSlot();
+            uim.slotFour.HideSaveSlot();
+        }
+
+        public void showAllSaves()
+        {
+            uim.slotOne.enabled = false;
+            uim.slotTwo.enabled = false;
+            uim.slotThree.enabled = false;
+            uim.slotFour.enabled = false;
+
+            uim.slotOne.enabled = true;
+            uim.slotTwo.enabled = true;
+            uim.slotThree.enabled = true;
+            uim.slotFour.enabled = true;
+
+            uim.slotOne.ShowSaveSlot();
+            uim.slotTwo.ShowSaveSlot();
+            uim.slotThree.ShowSaveSlot();
+            uim.slotFour.ShowSaveSlot();
+        }
+
+        public static void checkAddMaxPages(int x)
+        {
+            if (currentPage == (maxPages-1))
+                maxPages++;
+            PlayerPrefs.SetInt("MaxPages", maxPages);
+        }
+
+        public static void checkRemoveMaxPages(int x)
+        {
+            bool file = false;
+            if (currentPage == maxPages || currentPage == maxPages - 1)
+            {
+                for (int i = 1; i <= 8; i++)
+                {
+                    if (File.Exists(Application.persistentDataPath + "/user" + (((maxPages - 1) * 4) + i) + ".dat"))
+                        file = true;
+                }
+            }
+            if (!file)
+            {
+                maxPages--;
+                PlayerPrefs.SetInt("MaxPages", maxPages);
+                MoreSaves.pageLabel.text = String.Format("Page {0}/{1}", currentPage + 1, maxPages);
+            }
+
+        }
+
+        public static string getFilename(int x)
+        {
+            return "/user" + ((currentPage * 4) + x) + ".dat";
+        }
+    }
+}
